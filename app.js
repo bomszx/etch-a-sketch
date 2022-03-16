b0f4b20a424590ebd2d665381618f8def997ed77
bon nocos <bon.nocos@gmail.com>
2022-03-16 21:06:33 +0800

    added createGrid function

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-console.log('test')
+let div = document.createElement('div');
+
+function createGrid(number) {
+    for(i = 0; i < number * number; i++) {
+        let div = document.createElement('div');
+        let mainContainer = document.querySelector('.main-container');
+        div.className = 'grid';
+        mainContainer.append(div)
+    }
+} 
