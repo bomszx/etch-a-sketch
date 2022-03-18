3854a1b765bf11f67034a97122667d54112b9029
bon nocos <bon.nocos@gmail.com>
2022-03-18 12:42:23 +0800

    created hover function to color divs

--- app.js
+++ app.js
@@ -1,10 +1,6 @@
 let gridContainer = document.querySelector('.grid-container');
 let grid = document.querySelector('.grid');
 let gridItem = document.querySelector('.grid-item')
-
-// Add the eventlistener to the grid-container element
-gridContainer.addEventListener('mouseover', brush);
-
 let h1 = document.createElement('h1');
 
 // For some reason, I need to add a div that I can apply my grid templates to? Not create a div and apply the grid template in one go. I think the div has to be created first before we can applly our gridtemplates
@@ -13,7 +9,7 @@ function divMaker(num) {
         let div = document.createElement('div');
         div.className = 'grid-item';
         gridContainer.append(div);
-        console.log(div)
+        div.addEventListener('mouseover', brush)
     }
 }
 
@@ -26,8 +22,8 @@ function gridMaker (row, col) {
 }
 
 function brush(e) {
-    e.style.backgroundColor = 'red';
-    }
+    let color = [, "#55CBCD", "#E7993C",
+    "#869F9F", "#C6DFCD", "#A7BED3", "#C6E2E9", "#F1FFC4"];
 
-
-
+    this.style.backgroundColor = color[Math.floor(Math.random() * color.length)];
+}
