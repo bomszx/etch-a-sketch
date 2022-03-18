e9ef11cdc264df1bd67cdfae7e479ecdede0587e
bon nocos <bon.nocos@gmail.com>
2022-03-18 12:09:50 +0800

    added event listener for the grid

--- app.js
+++ app.js
@@ -1,28 +1,19 @@
 let gridContainer = document.querySelector('.grid-container');
 let grid = document.querySelector('.grid');
+let gridItem = document.querySelector('.grid-item')
+
+// Add the eventlistener to the grid-container element
+gridContainer.addEventListener('mouseover', brush);
+
 let h1 = document.createElement('h1');
 
-// div.className = 'grid-item';
-h1.innerText = 'testing'
-
-
-// I think we need to create the div first before we insert the grid templates
-
-// gridContainer.style.gridTemplateColumns = `repeat(4, 1fr)`;
-// gridContainer.style.gridTemplateRows = `repeat(4, 1fr)`;
-
-
-// 
-
-// For some reason, I need to add a div that I can apply my grid templates to? Not create a div and apply the grid template in one go.
-
+// For some reason, I need to add a div that I can apply my grid templates to? Not create a div and apply the grid template in one go. I think the div has to be created first before we can applly our gridtemplates
 function divMaker(num) {
     for(i = 0; i < num; i++) {
         let div = document.createElement('div');
         div.className = 'grid-item';
         gridContainer.append(div);
-
-        
+        console.log(div)
     }
 }
 
@@ -33,3 +24,10 @@ function gridMaker (row, col) {
         divMaker(col);
     }
 }
+
+function brush(e) {
+    e.style.backgroundColor = 'red';
+    }
+
+
+
