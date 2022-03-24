cb76cd646dfd94d8eebee8ec45af5b496d8de224
bon nocos <bon.nocos@gmail.com>
2022-03-24 22:11:03 +0800

    finished main logic, will add color palette next time

--- app.js
+++ app.js
@@ -1,15 +1,27 @@
-let gridContainer = document.querySelector('.grid-container');
 let grid = document.querySelector('.grid');
 let gridItem = document.querySelector('.grid-item')
-let h1 = document.createElement('h1');
+let gridContainer = document.querySelector('.grid-container');
+let resetButton = document.getElementById('reset')
+let blackButton = document.getElementById('black');
+let colorButton = document.getElementById('colored');
+
+// Reset
+resetButton.addEventListener('click', function() {
+    window.location.reload();
+    
+})
+
+
 
 // For some reason, I need to add a div that I can apply my grid templates to? Not create a div and apply the grid template in one go. I think the div has to be created first before we can applly our gridtemplates
+
 function divMaker(num) {
     for(i = 0; i < num; i++) {
         let div = document.createElement('div');
         div.className = 'grid-item';
-        gridContainer.append(div);
-        div.addEventListener('mouseover', brush)
+        div.value = 'cell';
+        gridContainer.append(div)
+        div.addEventListener('mouseover', blackBrush)
     }
 }
 
@@ -21,9 +33,34 @@ function gridMaker (row, col) {
     }
 }
 
-function brush(e) {
-    let color = [, "#55CBCD", "#E7993C",
-    "#869F9F", "#C6DFCD", "#A7BED3", "#C6E2E9", "#F1FFC4"];
+function blackBrush() {
+        this.style.backgroundColor = 'black';
+}
 
+function coloredBrush() {
+    let color = [, "#00a5e3", "#8dd7bf",
+    "#ff96c5", "#ff5768", "#ffbf65", "#fc6238", "#ffd872", "#f2d4cc", "#e77577", "#6c88c4", "#c05780", "#ff828b", "#f2d4cc", "#00b0ba", "#cff800", "#74737a", "#ffec59", "#00cdac", "fc6238", "#0065a2"];
+    
     this.style.backgroundColor = color[Math.floor(Math.random() * color.length)];
 }
+
+function changeColor() {
+    console.log(div)
+    div.removeEventListener('mouseover', blackBrush);
+    div.addEventListener('mouseover', coloredBrush);
+}
+
+function prompter() {
+    let num = prompt('Enter a number from 5-64', 64) 
+        if(num <= 5 || num >= 101) {
+            console.log(`num too big, we may crash`)
+        } else {
+            gridMaker(num,num)
+        }
+    }   
+
+
+colorButton.addEventListener('click', changeColor)
+blackButton.addEventListener('click', blackBrush)
+
+prompter() 
